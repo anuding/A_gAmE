751a1258c3bb4001c22b39f4fb715c75ccd18cb3
anu ding <anuding@126.com>
2018-12-21 16:17:40 +0800

    what

--- A_Game/GameObject.cpp
+++ A_Game/GameObject.cpp
@@ -6,6 +6,7 @@ GameObject::GameObject(D3DUtility* app)
 	buildEffect(device);
 	buildInputlayout(device);
 	buildVertexBufferandIndicesBuffer(device);
+	buildMaterialandLight();
 	SetWorldMatrix(XMMatrixIdentity());
 	
 	
@@ -58,16 +59,23 @@ void GameObject::buildEffect(ID3D11Device* device)
 void GameObject::buildInputlayout(ID3D11Device* device)
 {
 	HRESULT hr = S_OK;
-	technique = effect->GetTechniqueByIndex(0);
-	D3DX11_PASS_DESC PassDesc;
+	//用GetTechniqueByName获取ID3DX11EffectTechnique的对象
+//先设置默认的technique到Effect
+	technique = effect->GetTechniqueByName("T0");                //默认Technique
 
+	//D3DX11_PASS_DESC结构用于描述一个Effect Pass
+	D3DX11_PASS_DESC PassDesc;
+	//利用GetPassByIndex获取Effect Pass
+	//再利用GetDesc获取Effect Pass的描述，并存如PassDesc对象中
 	technique->GetPassByIndex(0)->GetDesc(&PassDesc);
+	
+	//创建并设置输入布局
+	//这里我们定义一个D3D11_INPUT_ELEMENT_DESC数组，
+	//由于我们定义的顶点结构包括位置坐标和法向量，所以这个数组里有两个元素
 	D3D11_INPUT_ELEMENT_DESC layout[] =
 	{
-		{"POSITION",0,DXGI_FORMAT_R32G32B32_FLOAT,0,0,
-			D3D11_INPUT_PER_VERTEX_DATA,0},
-		{"COLOR",0,DXGI_FORMAT_R32G32B32A32_FLOAT,0,
-			D3D11_APPEND_ALIGNED_ELEMENT,D3D11_INPUT_PER_VERTEX_DATA,0}
+		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0,  D3D11_INPUT_PER_VERTEX_DATA, 0},
+		{"NORMAL",   0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0}
 	};
 	UINT numElements = ARRAYSIZE(layout);
 
@@ -88,21 +96,44 @@ void GameObject::buildVertexBufferandIndicesBuffer(ID3D11Device* device)
 {
 	HRESULT hr = S_OK;
 	//2. Create Vertex Buffer and Indices Buffer
-	Vertex vertices[] = {
-		{XMFLOAT3(-1.0f,1.0f,-1.0f), XMFLOAT4(0.0f,0.0f,1.0f,1.0f)},
-		{XMFLOAT3(1.0f,1.0f,-1.0f), XMFLOAT4(0.0f,1.0f,0.0f,1.0f)},
-		{XMFLOAT3(1.0f,1.0f, 1.0f), XMFLOAT4(0.0f,1.0f,1.0f,1.0f)},
-		{XMFLOAT3(-1.0f,1.0f, 1.0f), XMFLOAT4(1.0f,0.0f,0.0f,1.0f)},
+		//和实验4一样，创建顶点数组，由于每个顶点有包含了坐标和颜色
+	//但是由于顶点的法向量不同，所以即使是同一个位置的三个顶点也必须单独定义
+	Vertex vertices[] =
+	{
+		{ XMFLOAT3(-1.0f, 1.0f, -1.0f), XMFLOAT3(0.0f, 1.0f, 0.0f) },
+		{ XMFLOAT3(1.0f, 1.0f, -1.0f), XMFLOAT3(0.0f, 1.0f, 0.0f) },
+		{ XMFLOAT3(1.0f, 1.0f, 1.0f), XMFLOAT3(0.0f, 1.0f, 0.0f) },
+		{ XMFLOAT3(-1.0f, 1.0f, 1.0f), XMFLOAT3(0.0f, 1.0f, 0.0f) },
 
-		{XMFLOAT3(-1.0f,-1.0f,-1.0f),XMFLOAT4(1.0f,0.0f,1.0f,1.0f)},
-		{XMFLOAT3(1.0f,-1.0f,-1.0f),XMFLOAT4(1.0f,1.0f,0.0f,1.0f)},
-		{XMFLOAT3(1.0f,-1.0f, 1.0f),XMFLOAT4(1.0f,1.0f,1.0f,1.0f)},
-		{XMFLOAT3(-1.0f,-1.0f, 1.0f),XMFLOAT4(0.0f,0.0f,0.0f,1.0f)},
+		{ XMFLOAT3(-1.0f, -1.0f, -1.0f), XMFLOAT3(0.0f, -1.0f, 0.0f) },
+		{ XMFLOAT3(1.0f, -1.0f, -1.0f), XMFLOAT3(0.0f, -1.0f, 0.0f) },
+		{ XMFLOAT3(1.0f, -1.0f, 1.0f), XMFLOAT3(0.0f, -1.0f, 0.0f) },
+		{ XMFLOAT3(-1.0f, -1.0f, 1.0f), XMFLOAT3(0.0f, -1.0f, 0.0f) },
+
+		{ XMFLOAT3(-1.0f, -1.0f, 1.0f), XMFLOAT3(-1.0f, 0.0f, 0.0f) },
+		{ XMFLOAT3(-1.0f, -1.0f, -1.0f), XMFLOAT3(-1.0f, 0.0f, 0.0f) },
+		{ XMFLOAT3(-1.0f, 1.0f, -1.0f), XMFLOAT3(-1.0f, 0.0f, 0.0f) },
+		{ XMFLOAT3(-1.0f, 1.0f, 1.0f), XMFLOAT3(-1.0f, 0.0f, 0.0f) },
+
+		{ XMFLOAT3(1.0f, -1.0f, 1.0f), XMFLOAT3(1.0f, 0.0f, 0.0f) },
+		{ XMFLOAT3(1.0f, -1.0f, -1.0f), XMFLOAT3(1.0f, 0.0f, 0.0f) },
+		{ XMFLOAT3(1.0f, 1.0f, -1.0f), XMFLOAT3(1.0f, 0.0f, 0.0f) },
+		{ XMFLOAT3(1.0f, 1.0f, 1.0f), XMFLOAT3(1.0f, 0.0f, 0.0f) },
+
+		{ XMFLOAT3(-1.0f, -1.0f, -1.0f), XMFLOAT3(0.0f, 0.0f, -1.0f) },
+		{ XMFLOAT3(1.0f, -1.0f, -1.0f), XMFLOAT3(0.0f, 0.0f, -1.0f) },
+		{ XMFLOAT3(1.0f, 1.0f, -1.0f), XMFLOAT3(0.0f, 0.0f, -1.0f) },
+		{ XMFLOAT3(-1.0f, 1.0f, -1.0f), XMFLOAT3(0.0f, 0.0f, -1.0f) },
+
+		{ XMFLOAT3(-1.0f, -1.0f, 1.0f), XMFLOAT3(0.0f, 0.0f, 1.0f) },
+		{ XMFLOAT3(1.0f, -1.0f, 1.0f), XMFLOAT3(0.0f, 0.0f, 1.0f) },
+		{ XMFLOAT3(1.0f, 1.0f, 1.0f), XMFLOAT3(0.0f, 0.0f, 1.0f) },
+		{ XMFLOAT3(-1.0f, 1.0f, 1.0f), XMFLOAT3(0.0f, 0.0f, 1.0f) },
 	};
 	UINT vertexCount = ARRAYSIZE(vertices);
 	D3D11_BUFFER_DESC bd; ZeroMemory(&bd, sizeof(bd));
 	bd.Usage = D3D11_USAGE_DEFAULT;
-	bd.ByteWidth = sizeof(Vertex) * 8;
+	bd.ByteWidth = sizeof(Vertex) * 24;
 	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
 	bd.CPUAccessFlags = 0;
 
@@ -119,8 +150,8 @@ void GameObject::buildVertexBufferandIndicesBuffer(ID3D11Device* device)
 
 	WORD indices[] =
 	{
-		3,1,0, 2,1,3, 0,5,4, 1,5,0, 3,4,7, 0,4,3,
-		1,6,5, 2,6,1, 2,7,6, 3,7,2, 6,4,5, 7,4,6,
+		3,1,0,    2,1,3,    6,4,5,    7,4,6,    11,9,8,    10,9,11,
+		14,12,13, 15,12,14, 19,17,16, 18,17,19, 22,20,21,  23,20,22
 	};
 	UINT indexCount = ARRAYSIZE(indices);
 
@@ -139,6 +170,42 @@ void GameObject::buildVertexBufferandIndicesBuffer(ID3D11Device* device)
 		::MessageBox(NULL, L"Index Buffer creation failed!!", L"ERROR", MB_OK);
 		
 	}
+}
+
+void GameObject::buildMaterialandLight()
+{
+	//*************第四步设置材质和光照***********************
+	// 设置材质：3中光照的反射率以及镜面光反射系数
+	//反射率中前三位表示红绿蓝光的反射率，1表示完全反射，0表示完全吸收
+	material.ambient = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f); //前三位分别表示红绿蓝光的反射率
+	material.diffuse = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f); //同上
+	material.specular = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);//同上
+	material.power = 5.0f;
+	// 设置光源
+	Light dirLight;
+	// 方向光只需要设置：方向、3种光照强度
+	dirLight.type = 0;
+	dirLight.direction = XMFLOAT4(-1.0f, 0.0f, 1.0f, 1.0f); //光照方向
+	dirLight.ambient = XMFLOAT4(0.2f, 0.2f, 0.2f, 1.0f);   //前三位分别表示红绿蓝光的强度
+	dirLight.diffuse = XMFLOAT4(0.3f, 0.3f, 0.3f, 1.0f);   //同上
+	dirLight.specular = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);  //同上
+	light = dirLight;
+	//材质信息的常量缓存中的材质信息设置到Effect框架中-----------------------------
+	effect->GetVariableByName("MatAmbient")->AsVector()->SetFloatVector((float*)&(material.ambient));
+	effect->GetVariableByName("MatDiffuse")->AsVector()->SetFloatVector((float*)&(material.diffuse));
+	effect->GetVariableByName("MatSpecular")->AsVector()->SetFloatVector((float*)&(material.specular));
+	effect->GetVariableByName("MatPower")->AsScalar()->SetFloat(material.power);
+
+	//首先将光照类型，环境光强度，漫射光强度，镜面光强度设置到Effect中
+	effect->GetVariableByName("type")->AsScalar()->SetInt(light.type);
+	effect->GetVariableByName("LightAmbient")->AsVector()->SetFloatVector((float*)&(light.ambient));
+	effect->GetVariableByName("LightDiffuse")->AsVector()->SetFloatVector((float*)&(light.diffuse));
+	effect->GetVariableByName("LightSpecular")->AsVector()->SetFloatVector((float*)&(light.specular));
+
+	//方向光只需要“方向”这个属性即可
+	effect->GetVariableByName("LightDirection")->AsVector()->SetFloatVector((float*)&(light.direction));
+	//将方向光的Tectnique设置到Effect
+	technique = effect->GetTechniqueByName("T_DirLight");
 }
 
 bool GameObject::Setup()
--- A_Game/GameObject.h
+++ A_Game/GameObject.h
@@ -7,7 +7,39 @@
 struct  Vertex
 {
 	XMFLOAT3 Pos;
-	XMFLOAT4 Color;
+	XMFLOAT3 Normal;
+};
+//这是材质的对各种光反射率
+struct Material
+{
+	XMFLOAT4 ambient;		//材质环境光反射率
+	XMFLOAT4 diffuse;		//材质漫射光反射率
+	XMFLOAT4 specular;		//材质镜面光反射率
+	float    power;			//镜面光反射系数
+};
+
+//光源结构，这个结构包括了3种光源的所有属性
+//但不是每种属性都会用到，比如方向光就不会用到
+//光源位置，以及衰减因子等
+struct Light
+{
+	int type;				//光源类型，方向光：0，点光源：1，聚光灯：2
+
+	XMFLOAT4 position;		//光源位置
+	XMFLOAT4 direction;		//方向向量
+
+	XMFLOAT4 ambient;		//环境光强度
+	XMFLOAT4 diffuse;		//漫射光强度
+	XMFLOAT4 specular;		//镜面光强度
+
+	float attenuation0;		//常量衰减因子
+	float attenuation1;		//一次衰减因子
+	float attenuation2;		//二次衰减因子
+
+	float alpha;			//聚光灯内锥角度
+	float beta;				//聚光灯外锥角度
+	float fallOff;			//聚光灯衰减系数，一般取值为1.0
+
 };
 class GameObject
 {
@@ -38,10 +70,13 @@ public:
 	D3DX11_TECHNIQUE_DESC techDesc;
 	D3DUtility* mapp;
 private:
-	
+	//声明材质和光照的全局对象
+	Material		material;      //材质
+	Light			light;      //光源数组
 	void buildEffect(ID3D11Device* device);
 	void buildInputlayout(ID3D11Device* device);
 	void buildVertexBufferandIndicesBuffer(ID3D11Device* device);
+	void buildMaterialandLight();
 	bool Setup();
 };
 
